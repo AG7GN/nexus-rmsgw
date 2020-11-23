10fc8b742495a2f9efb13f414ee14b27ef17f6da
Steve Magnuson <ag7gn.steve@gmail.com>
2020-11-23 15:47:30 -0800

    Update install-rmsgw.sh

--- install-rmsgw.sh
+++ install-rmsgw.sh
@@ -110,6 +110,7 @@ echo "Retrieve the latest rmsgw software"
 sudo mkdir -p /etc/rmsgw
 [ -d /usr/local/etc/rmsgw ] && sudo rm -rf /usr/local/etc/rmsgw
 sudo ln -s /etc/rmsgw /usr/local/etc/rmsgw
+URL="https://github.com/nwdigitalradio/rmsgw"
 DIR_="rmsgw"
 UP_TO_DATE=$FALSE
 if ! [[ -s $SRC_DIR/$GIT_DIR/.git/HEAD ]]
